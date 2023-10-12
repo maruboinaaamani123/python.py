ec64c6737a4f80dadd4fa4cd8adbe372fc9072c0
maruboinaaamani123 <113514722+maruboinaaamani123@users.noreply.github.com>
2023-10-12 20:52:19 +0530

    Add files via upload

--- fibonacci_series.py
+++ fibonacci_series.py
@@ -1,3 +1,4 @@
+"""
 n=int(input("enter number of terms"))
 a=0
 b=1
@@ -7,4 +8,24 @@ for i in range(2,n):
     print(",",c,end="")
     a=b
     b=c
-
+"""
+"""
+n = 10
+a = 1
+b = 0
+i = 1
+while(i <= n):
+    print(b)
+    a = a+b
+    b = a-b
+    i += 1
+"""
+n = 20
+a = 1
+b = 0
+i = 1
+while(i <= n):
+    print(b)
+    a =a+b
+    b =a-b
+    i +=1
