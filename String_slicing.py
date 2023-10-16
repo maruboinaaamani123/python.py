14a113b36d05dbbb80dc429bb37dc5dfcab5bea1
maruboinaaamani123 <113514722+maruboinaaamani123@users.noreply.github.com>
2023-10-16 15:19:31 +0530

    Add files via upload

---
+++ String_slicing.py
@@ -0,0 +1,32 @@
+#string slicing
+name = "Aamani"
+print(name[0:3])
+print(name[:6])
+print(name[0:])
+print(name[0:5])
+
+#Negative String slicing
+name = "python"
+print(name[-6:-1])
+print(name[-6:-3])
+print(name[::-1])
+
+#reverse a list items
+name = ["Aamani","teja","anji","ammu"]
+name.reverse()
+print(name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
